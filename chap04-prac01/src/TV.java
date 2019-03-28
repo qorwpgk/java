ec4a0fb49949f3b93e004fe1741f4d6d00a83e02
USER <USER@AMP-09>
2019-03-28 10:53:11 +0900

    Commit

--- chap04-prac01/src/TV.java
+++ chap04-prac01/src/TV.java
@@ -1,9 +1,23 @@
 
 public class TV {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		private String manufacturer;
+		private int year;
+		private int size;
+		
+		public TV(String manufacturer,int year,int size){
+			this.manufacturer = manufacturer;
+			this.year =year;
+			this.size =size;
 	}
+		public void show(){
+			System.out.print(this.manufacturer + "에서 만든 ");
+			System.out.print(this.year + "년형");
+			System.out.print(this.size + "인치 TV");
+		}
+		public static void main(String [] args){
+			TV myTV = new TV("LG",2017,32);
+			myTV.show();
+		}
 
 }
