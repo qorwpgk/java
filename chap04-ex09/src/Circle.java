5757b60c04d87b0768681ca243bfdef5d410f54b
USER <USER@AMP-27>
2019-04-18 12:17:33 +0900

    commit

---
+++ chap04-ex09/src/Circle.java
@@ -0,0 +1,32 @@
+
+public class Circle {
+int radius;
+String name;
+private Object dount;
+
+public Circle(){
+	
+	
+}
+
+public double getArea(){
+	return 3.14 * radius * radius;
+	
+}
+
+public static void main(String[]args){
+	Circle pizza;
+	pizza = new Circle();
+	pizza.radius = 5;
+	pizza.name = "책을피자";
+	double area = pizza.getArea();
+	System.out.println(pizza.name + "의 면적"+ area);
+	
+	Circle donut = new Circle();
+	donut.radius = 3;
+	donut.name = "머리";
+	area = donut.getArea();
+	System.out.println(donut.name+ "의 면적");
+	
+}
+}
