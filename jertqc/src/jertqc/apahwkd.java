3c74c3f8839ceee22ae9044e6eb33a58c6ccea5f
qorwpgk <48539270+qorwpgk@users.noreply.github.com>
2019-04-20 14:30:35 +0900

    Update apahwkd.java

--- jertqc/src/jertqc/apahwkd.java
+++ jertqc/src/jertqc/apahwkd.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class apahwkd extends JFrame{
 	public apahwkd() {
-		setTitle("¸Ş¸ğÀå");
+		setTitle("ë©”ëª¨ì¥");
 		createMenu();
 		createToolBar();
 		createSplitPane();
@@ -16,11 +16,11 @@ public class apahwkd extends JFrame{
 
 	private void createMenu() {
 		JMenuBar mb = new JMenuBar();
-		mb.add(new JMenu("ÆÄÀÏ(F)"));
-		mb.add(new JMenu("ÆíÁı(E)"));
-		mb.add(new JMenu("¼­½Ä(O)"));
-		mb.add(new JMenu("º¸±â(H)"));
-		mb.add(new JMenu("µµ¿ò¸»(H)"));
+		mb.add(new JMenu("íŒŒì¼(F)"));
+		mb.add(new JMenu("í¸ì§‘(E)"));
+		mb.add(new JMenu("ì„œì‹(O)"));
+		mb.add(new JMenu("ë³´ê¸°(H)"));
+		mb.add(new JMenu("ë„ì›€ë§(H)"));
 		
 		this.setJMenuBar(mb);
 	}
@@ -28,33 +28,33 @@ public class apahwkd extends JFrame{
 	private void createToolBar() {
 		JToolBar bar = new JToolBar("Kitae Menu");
 		
-		bar.setBackground(Color.LIGHT_GRAY);
-		bar.add(new JButton("New"));
-		bar.add(new JButton(new ImageIcon("images/open.jpg")));
-		bar.addSeparator();
-		bar.add(new JButton(new ImageIcon("images/open.jpg")));
-		bar.add(new JLabel("search"));
-		bar.add(new JTextField("text field"));
+		//bar.setBackground(Color.LIGHT_GRAY);
+		//bar.add(new JButton("New"));
+		//bar.add(new JButton(new ImageIcon("images/open.jpg")));
+		//bar.addSeparator();
+		//bar.add(new JButton(new ImageIcon("images/open.jpg")));
+		//bar.add(new JLabel("search"));
+		//bar.add(new JTextField("text field"));
 		
-		JComboBox<String> combo = new JComboBox<String>();
-		combo.addItem("Java");
-		combo.addItem("C#");
-		combo.addItem("C");
-		combo.addItem("C++");
-		bar.add(combo);
-		add(bar,BorderLayout.NORTH);
+		//JComboBox<String> combo = new JComboBox<String>();
+		//combo.addItem("Java");
+		//combo.addItem("C#");
+		//combo.addItem("C");
+		//combo.addItem("C++");
+		//bar.add(combo);
+		//add(bar,BorderLayout.NORTH);
 		
 	}
 	
 	private void createSplitPane() {
-	String[] fruits = {"apple","banana","kiwi","mango",
-			"peach","berry","stawberry","blackberry"};
+	//String[] fruits = {"apple","banana","kiwi","mango",
+			//"peach","berry","stawberry","blackberry"};
 	
-	JList<String>scrollList = new JList<String>(fruits);
-	JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
-			new JScrollPane(scrollList),new JScrollPane(new JLabel(new ImageIcon("images/mg1.jpg"))));
+	//JList<String>scrollList = new JList<String>(fruits);
+	//JSplitPane pane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+			//new JScrollPane(scrollList),new JScrollPane(new JLabel(new ImageIcon("images/mg1.jpg"))));
 	
-	add(pane, BorderLayout.CENTER);
+	//add(pane, BorderLayout.CENTER);
 	}
 	public static void main(String[]args) {
 		new apahwkd();
