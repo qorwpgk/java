c94a93ea42a9b7d09a69a7ca7ed7209f002c06f7
USER <USER@AMP-27>
2019-05-30 12:40:40 +0900

    commit

---
+++ chape13-ex03/src/FlickeringLabelEx.java
@@ -0,0 +1,58 @@
+import java.awt.*;
+import javax.swing.*;
+
+class FlickeringLabel extends JLabel implements Runnable{
+	private long delay;
+	public FlickeringLabel(String text, long delay){
+		super(text);
+		this.delay = delay;
+		setOpaque(true);
+		Thread th = new Thread(this);
+		th.start();
+	}
+	@Override
+	public void run(){
+		int n =0;
+		while(true){
+			if(n == 0)
+				setBackground(Color.YELLOW);
+			else
+				setBackground(Color.GREEN);
+			if(n == 0)n = 1;
+			else n =0;
+			try{
+				Thread.sleep(delay);
+			}
+			catch(InterruptedException e){
+				return;
+			}
+		}
+	}
+}
+public class FlickeringLabelEx extends JFrame{
+	public FlickeringLabelEx(){
+		setTitle("FlickeringLabelEx ¿¹Á¦");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+	    c.setLayout(new FlowLayout());
+	    
+	    FlickeringLabel fLabel = new FlickeringLabel("±ô¹Ú",500);
+	    
+	    JLabel label = new JLabel("¾È±ô¹Ú");
+	    
+	    FlickeringLabel flLabel2 = new FlickeringLabel("¿©±âµµ ±ô¹Ú",300);
+	    
+	    c.add(fLabel);
+	    c.add(label);
+	    c.add(flLabel2);
+	    
+	    setSize(300,150);
+	    setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          new FlickeringLabelEx();
+	}
+
+}
