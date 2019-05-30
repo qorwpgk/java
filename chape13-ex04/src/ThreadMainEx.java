e770fee77fc051263df7138569512b73503fb223
USER <USER@AMP-27>
2019-05-30 13:52:57 +0900

    commit

---
+++ chape13-ex04/src/ThreadMainEx.java
@@ -0,0 +1,15 @@
+
+public class ThreadMainEx {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      long id = Thread.currentThread().getId();
+      String name = Thread.currentThread().getName();
+      int priority = Thread.currentThread().getPriority();
+      Thread.State s = Thread.currentThread().getState();
+      System.out.println("현재 스레드 이름 = "+ name);
+      System.out.println("현재 스레드ID =" + id);
+     System.out.println("현재 스레드 우선순의 값 ="+ priority);
+     System.out.println("현재 스레드 상태 ="+ s);
+	}
+
+}
