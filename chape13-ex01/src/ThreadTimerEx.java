53bdfe6b61ada737ae7cbe4ab737a1d0455a4eaf
USER <USER@AMP-27>
2019-05-30 12:15:42 +0900

    commit

---
+++ chape13-ex01/src/ThreadTimerEx.java
@@ -0,0 +1,50 @@
+import java.awt.*;
+import javax.swing.*;
+
+class TimerTherad extends Thread{
+	private JLabel timerLabel;
+	
+	public TimerTherad(JLabel timerLabel){
+		this.timerLabel = timerLabel;
+	}
+	
+	@Override
+	public void run(){
+		int n = 0;
+	while(true){
+		timerLabel.setText(Integer.toString(n));
+		n++;
+		try{
+			Thread.sleep(1000);
+		 }
+		catch (InterruptedException e) {
+			return ;
+		  }
+		}
+	 }
+}
+public class ThreadTimerEx extends JFrame{
+	public ThreadTimerEx(){
+		setTitle("Thread를 상속받은 타이머 스레드 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c =getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		JLabel timerLabel = new JLabel();
+		timerLabel.setFont(new Font("Gothic", Font.ITALIC,80));
+		c.add(timerLabel);
+		
+		TimerTherad th = new TimerTherad(timerLabel);
+		
+		setSize(300,170);
+		setVisible(true);
+		
+		th.start();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          new ThreadTimerEx();
+	}
+
+}
